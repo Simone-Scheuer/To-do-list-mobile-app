3fbcdcb08ebb60dc88980408a659f5af7841b883
Simone Scheuer <156980303+Simone-Scheuer@users.noreply.github.com>
2024-09-03 14:35:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,58 +18,18 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
-import { getDatabase,
-         ref,
-         push,
-         onValue,
-         remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
-
-const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-a5622-default-rtdb.firebaseio.com/"
-}
-
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-const referenceInDB = ref(database, "leads")
-
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         let itemNbox = leads[i] += '<input type="checkbox"></input>'
-        listItems +=  
-         `
+        listItems += `
             <li>
                 ${itemNbox}
             </li>
-         `
+        `
     }
     ulEl.innerHTML = listItems
 }
-
-onValue(referenceInDB, function(snapshot) {
-    const snapshotDoesExist = snapshot.exists()
-    if (snapshotDoesExist) {
-        const snapshotValues = snapshot.val()
-        const leads = Object.values(snapshotValues)
-        render(leads)
-    }
-})
-
-deleteBtn.addEventListener("dblclick", function() {
-    remove(referenceInDB)
-    ulEl.innerHTML = ""
-})
-
-inputBtn.addEventListener("click", function() {
-    push(referenceInDB, inputEl.value)
-    inputEl.value = "" 
-})
 
 onValue(referenceInDB, function(snapshot) {
     const snapshotDoesExist = snapshot.exists()
